9dd4a98869dac5ce791a00ac48b3a3aea69b55cf
Jeffrey Varner <jdv27@cornell.edu>
2024-02-01 12:18:35 -0500

    Update Compute.jl
    comments

--- week-2/Lab-2d/src/Compute.jl
+++ week-2/Lab-2d/src/Compute.jl
@@ -3,6 +3,10 @@
 function find(students::Array{MyStudentModel,1}; 
     sid::Int64=0, netid::String="abc123")::Union{Int64, Nothing}
 
+    # check: should we check to see if the parameters are valid?
+    # Let's discuss this in the testing phase
+
+
     # initialize -
     student_index = nothing; # default: we don't know which student we are looking for
     number_of_students = length(students) # how many students do we have?
