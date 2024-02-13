51c22b176c9a16d63964fef24af1790324ffa76a
Jeffrey Varner <jdv27@cornell.edu>
2024-02-13 12:04:34 -0500

    changes
    changes

--- week-4/L4b/src/Factory.jl
+++ week-4/L4b/src/Factory.jl
@@ -6,14 +6,5 @@
     build(base::String, model::MyWeatherGridPointEndpointModel) -> String
 """
 function build(base::String, model::MyWeatherGridPointEndpointModel)::String
-
-    # get data -
-    latitude = model.latitude;
-    longitude = model.longitude;
-
-    # build the base url -
-    url_string = "$(base)/points/$(latitude),$(longitude)";
-
-    # return -
-    return url_string;
+    return "-1";
 end
--- week-4/L4b/src/Types.jl
+++ week-4/L4b/src/Types.jl
@@ -1,14 +1,6 @@
 abstract type AbstractWeatherEndpointModel end
 
 mutable struct MyWeatherGridPointEndpointModel <: AbstractWeatherEndpointModel
-    
-    # data -
-    latitude::Float64
-    longitude::Float64
-
-    # methods -
-    MyWeatherGridPointEndpointModel(; 
-        latitude::Float64 = 0.0, longitude::Float64 = 0.0) = new(latitude, longitude);
 end
 
 mutable struct MyWeatherForecastEndpointModel <: AbstractWeatherEndpointModel
