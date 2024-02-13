43e5005b8d0a501750058d22fca7b6a2eca4e70a
Jeffrey Varner <jdv27@cornell.edu>
2024-02-13 15:34:15 -0500

    Types
    Types

--- week-4/L4b/src/Factory.jl
+++ week-4/L4b/src/Factory.jl
@@ -11,6 +11,12 @@ It takes two arguments, a base URL string, and a model of type `MyWeatherGridPoi
 function build(base::String, model::MyWeatherGridPointEndpointModel)::String
     
     # TODO: implement this function, and remove the throw statement
-    throw(ArgumentError("build(base::String, model::MyWeatherGridPointEndpointModel) not implemented yet!"));
+    # throw(ArgumentError("build(base::String, model::MyWeatherGridPointEndpointModel) not implemented yet!"));
+
+    # build the URL string -
+    url_string = "$(base)/points/$(model.latitude),$(model.longitude)";
+
+    # return the URL string -
+    return url_string;
 end
 # --- PUBLIC METHODS ABOVE HERE -------------------------------------------------------------------------------- #
