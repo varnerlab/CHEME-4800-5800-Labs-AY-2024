74194ca481e9f6d18a9a27bba74c5f27f5d83299
Jeffrey Varner <jdv27@cornell.edu>
2024-02-20 11:24:50 -0500

    Update Parser.jl
    removed all the comments

--- week-5/Lab-5b/src/Parser.jl
+++ week-5/Lab-5b/src/Parser.jl
@@ -1,48 +1,28 @@
 """
     _recursive_descent_parser(q::Queue, tmp::Queue{Char}, a::Array{String,1}; 
-        delim = ' ')
+        delim = ' ')::Nothing
 """
 function _recursive_descent_parser(q::Queue, tmp::Queue{Char}, a::Array{String,1}; 
-    delim = ' ')
-
-    # base case: we have no more characters in the character_arr - we are done
+    delim = ' ')::Nothing
     if (isempty(q) == true)
-        
-        # if we have any remaing stuff in tmp, then join, and add
         if (isempty(tmp) == false)
             word = join(tmp)
             if (isempty(word) == false)
-                push!(a,word) # add that word to our word list -
+                push!(a,word)
             end
         end
-        
-        # done -
         return nothing 
     else
-
-        # grab the next_char -
         next_char = dequeue!(q)
         if (next_char == delim)
-            
-            # if we get here, then we have hit a delim character, this means we should
-            # turn the characters in the s array into a word
-            # join chars in the character array -
             word = join(tmp)
             if (isempty(word) == false)
-                push!(a, word) # add that word to our word list -
+                push!(a, word)
             end
-
-            # empty out the array of characters, because we may need it again
             empty!(tmp);
         else
-
-            # if we get here, next_char is *not* the delim, so push next_char into the array
-            # Why? we are collecting next_char until we hit a delim or hit the base case
-            # When we hit a delim the characters in s can be joined to make a word
             enqueue!(tmp, next_char)
         end
-
-        # process the next character in the queue -
         _recursive_descent_parser(q, tmp, a; delim = delim);
     end
 end
@@ -59,21 +39,14 @@ function recursivesplit(string::String; delim::Char=' ')::Dict{Int64,String}
     a = Array{String,1}();
     counter = 0
 
-    # build the Queue q that we are going to parse -
     character_arr = collect(string)
     for c ∈ character_arr
         enqueue!(q, c);
     end
-
-    # recursive descent -
     _recursive_descent_parser(q, tmp, a; delim = delim);
-
-    # convert to dictionary for the output
     for item ∈ a
         d[counter] = item;
         counter += 1
     end
-
-    # return -
     return d
 end
