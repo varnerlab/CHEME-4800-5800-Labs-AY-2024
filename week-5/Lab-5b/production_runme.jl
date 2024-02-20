7e8d94d98829abe6d2e3fc0ad1ac05e694e3bd7e
Jeffrey Varner <jdv27@cornell.edu>
2024-02-20 11:29:53 -0500

    Update production_runme.jl
    put TODO sections

--- week-5/Lab-5b/production_runme.jl
+++ week-5/Lab-5b/production_runme.jl
@@ -7,32 +7,11 @@ path_to_reactions_file = joinpath(_PATH_TO_DATA, "Reactions.net")
 # Step 2: parse the list of chemical reaction strings -
 reactions = read_reaction_file(path_to_reactions_file);
 
-# Make the reaction name array =
-reaction_name_array = Array{String,1}();
-for (k, v) in reactions
-    push!(reaction_name_array, k);
-end
+# TODO: Build the reaction_name_array -
+# ...
 
-# Generate a species list from the reaction strings -
-species = Array{String, 1}();
-for i ∈ eachindex(reaction_name_array)
-    reaction_model = reactions[reaction_name_array[i]];
-    
-    # process the reactants -
-    d = recursivesplit(reaction_model.reactants, delim='+');
-    for (k,v) ∈ d
-        if (v ∉ species)
-            push!(species, v)
-        end
-    end
+# TODO: Build the species_formula_array -
+# ...
 
-    # process the products -
-    d = recursivesplit(reaction_model.products, delim='+');
-    for (k,v) ∈ d
-        if (v ∉ species)
-            push!(species, v)
-        end
-    end
-end
-
-# Step 3: Cut the reaction and product strings around the +'s (using recursive descent parser)
+# TODO: Challenge problem: build the stoichiometry_matrix
+# ...
