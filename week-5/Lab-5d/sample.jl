09fc788d9c17ae9244300f5793e01fc35e01a27f
Jeffrey Varner <jdv27@cornell.edu>
2024-02-22 13:05:49 -0500

    sample w/random IC
    sample w/random IC

--- week-5/Lab-5d/sample.jl
+++ week-5/Lab-5d/sample.jl
@@ -7,13 +7,15 @@ number_of_columns = 256;
 middle = (number_of_rows / 2) |> Int
 number_of_iterations = 960;
 exportonlyeven = false;
+randomcutoff = -0.1; # no noise if negative
+
 
 # build a rule -
 rule = Dict{Int64,Int64}();
 rule[1] = 1; # average 0.0 
 rule[2] = 0; # average 0.25 
 rule[3] = 1; # average 0.50 
-rule[4] = 1; # average 0.75
+rule[4] = 0; # average 0.75
 rule[5] = 1; # average 1.0
 rulemodel = build(MyTwoDimensionalGridWorldRuleModel, rule);
 
@@ -23,6 +25,8 @@ for row ∈ 1:number_of_rows
     for column ∈ 1:number_of_columns
         if (row == middle && column == middle)
             state[(row, column)] = 1;
+        elseif (rand() < randomcutoff)
+            state[(row, column)] = 1; # add a little bit of noise
         else
             state[(row, column)] = 0;
         end
