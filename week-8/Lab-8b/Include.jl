0d16c002bae716c8c82c7f196d2530a67b162d72
Jeffrey Varner <jdv27@cornell.edu>
2024-03-10 14:15:52 -0400

    Working on Lab 8b
    Lab 8b

---
+++ week-8/Lab-8b/Include.jl
@@ -0,0 +1,18 @@
+# setup paths -
+const _ROOT = @__DIR__
+const _PATH_TO_SRC = joinpath(_ROOT, "src")
+const _PATH_TO_DATA = joinpath(_ROOT, "data")
+
+# check: do we have the required packahes loaded??
+using Pkg
+Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+
+# load external packages -
+using LinearAlgebra; 
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Types.jl"));
+include(joinpath(_PATH_TO_SRC, "Factory.jl"));
+include(joinpath(_PATH_TO_SRC, "Files.jl"));
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
+include(joinpath(_PATH_TO_SRC, "Solvers.jl"));
