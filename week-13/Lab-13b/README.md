50af839ce65b04cbc3cedce9ad1b73ac9837e819
Jeffrey Varner <jdv27@cornell.edu>
2024-04-17 16:37:49 -0400

    HMM example
    Hmmm example

---
+++ week-13/Lab-13b/README.md
@@ -0,0 +1,2 @@
+## We worked on Problem Set 5
+We worked on the build method for constructing the bipartite graph model
---
+++ week-13/Lab-13d/src/Factory.jl
@@ -0,0 +1,26 @@
+function build(model::Type{M}, data::NamedTuple)::AbstractMarkovModel where {M <: AbstractMarkovModel}
+    
+    # initialize -
+    m = model(); # build an empty model, add data to it below
+    transition = Dict{Int64, Categorical}();
+    emission = Dict{Int64, Categorical}();
+
+    # get stuff from the data NamedTuple -
+    states = data.states;
+    T = data.T; # this is the transition matrix
+    E = data.E; # this is the emission matrix
+
+    # build the transition and emission distributions -
+    for s ∈ states
+        transition[s] = Categorical(T[s,:]);
+        emission[s] = Categorical(E[s,:]);
+    end
+
+    # add data to the model -
+    m.transition = transition;
+    m.emission = emission;
+    m.states = states;
+
+    # return -
+    return m;
+end
